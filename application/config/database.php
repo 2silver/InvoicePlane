f37c5df5a9a3a67c64f73423edf70735c14213a5
nscgraf <michael.graf@fhstp.ac.at>
2014-09-24 15:47:48 +0200

    add german language, database settings for localhost, .gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'invoice';
+$db['default']['password'] = 'invoice';
+$db['default']['database'] = 'invoiceplane';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
